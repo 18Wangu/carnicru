a46a4e87539b23090655c17e4a69ae27bff54ae7
Theo <theopremartin@orange.fr>
2024-06-08 14:48:26 +0200

    c'est repartie pour tout recoder, un plaisir !

--- app/pages/faq/page.tsx
+++ app/pages/faq/page.tsx
@@ -1,3 +1,4 @@
+import Formule from "@/app/ui/formule/formule";
 import { PageIndisponible } from "@/app/ui/page-indisponible";
 
 export default function Faq() {
@@ -5,6 +6,9 @@ export default function Faq() {
         <main>
             <div>
                 <PageIndisponible />
+            </div>
+            <div>
+                <Formule />
             </div>
         </main>
     );  
--- app/ui/formule/formule.tsx
+++ app/ui/formule/formule.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 export default function Formule() {
     return (
-        <div>
-            <h1 className='text-2xl text-[#004339] text-center m-4'>Formule mensuelle</h1>
+        <div className='flex flex-col items-center'>{/* nativement ca prend toute la largeur de l'ecran, peut etre mettre une with pour restrindre */}
+            <h1 className='text-2xl text-[#004339] m-4'>Formule mensuelle</h1>
             <Image
-                src="/formule-chien-image.svg"
+                src="/formule-img-chien.svg"
                 alt="Formule mensuelle"
                 width={350}
                 height={280}
             />
-            <div className='bg-[#E30613] rounded-2xl m-4'>
+            <div className='bg-[#E30613] rounded-2xl m-4 w-80'>
                 <button className='p-5 text-[#F8F9E9] text-2xl'>Commander</button> {/* je pense qu'il faudra remplacer le button par un link */}
             </div>
         </div>
